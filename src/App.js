a0ef055339295bfdcb0c00b46111d12882968c05
DikuCoding <dikeshkhagi1@gmail.com>
2024-11-11 12:59:13 +0545

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Footer from "./components/Footer";
+
 function App() {
   return (
+    <>
     <h1 className="text-3xl font-bold underline bg-red-500">Hello world!</h1>
+    <Footer/>
+    </>
   );
 }
 
