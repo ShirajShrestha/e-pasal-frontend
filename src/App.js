d30546b44f1115a6bada65dc4fb71e6b6fde2f02
DikuCoding <dikeshkhagi1@gmail.com>
2024-11-12 11:23:14 +0545

    add card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import Card from "./components/Card";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline bg-red-500">Hello world!</h1>
-    <Footer/>
+    <Card/>
     </>
   );
 }
