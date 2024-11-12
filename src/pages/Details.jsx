56ff64e321b3cc9d7caf0d679abfa1692e9ee702
Shiraj Shrestha <shresthashiraj333@gmail.com>
2024-11-13 01:47:42 +0545

    Fix width and margin of details and descriptions

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -16,7 +16,7 @@ const Details = () => {
   };
 
   return (
-    <div>
+    <div className="my-4">
       {/* Image and details section  */}
       <div className="flex flex-col lg:flex-row m-4 lg:mx-20">
         <div className="flex-1 ">
@@ -27,47 +27,49 @@ const Details = () => {
           />
         </div>
         <div className="flex-1  p-4 font-semibold">
-          <p className="font-bold text-2xl">
-            Nike Dri-Fit Training Jacket Winter Special Kit
-          </p>
-          <div className="flex items-center justify-between pb-4">
-            <p className="font-semibold text-gray-600 ml-2 ">Brand: Nike</p>
-            <FiHeart className="text-xl cursor-pointer hover:text-accent" />
-          </div>
-          <p className="text-xl font-bold">Rs 900</p>
-          <p className="mb-4">
-            <span className="line-through text-gray-500">Rs 1000 </span>
-            <span className="text-red-600"> 10&#37; discount</span>
-          </p>
-          <p className="text-gray-500 mb-4">
-            Stock: <span className="text-black">5</span>{" "}
-          </p>
-          <div className="mb-4">
-            <p className="font-bold mb-1">Qty</p>
-            <div>
-              <span
-                className="border border-black px-2 py-1 rounded cursor-pointer"
-                onClick={decreaseQuantity}
-              >
-                -
-              </span>
-              <span className="m-1">{quantity}</span>
-              <span
-                className="border border-black px-2 py-1 rounded cursor-pointer"
-                onClick={increaseQuantity}
-              >
-                +
-              </span>
+          <div className="lg:w-3/4">
+            <p className="font-bold text-2xl">
+              Nike Dri-Fit Training Jacket Winter Special Kit
+            </p>
+            <div className="flex items-center justify-between pb-4">
+              <p className="font-semibold text-gray-600 ml-2 ">Brand: Nike</p>
+              <FiHeart className="text-xl cursor-pointer hover:text-accent mr-10" />
             </div>
+            <p className="text-xl font-bold">Rs 900</p>
+            <p className="mb-4">
+              <span className="line-through text-gray-500">Rs 1000 </span>
+              <span className="text-red-600"> 10&#37; discount</span>
+            </p>
+            <p className="text-gray-500 mb-4">
+              Stock: <span className="text-black">5</span>{" "}
+            </p>
+            <div className="mb-4">
+              <p className="font-bold mb-1">Qty</p>
+              <div>
+                <span
+                  className="border border-black px-2 py-1 rounded cursor-pointer"
+                  onClick={decreaseQuantity}
+                >
+                  -
+                </span>
+                <span className="m-1">{quantity}</span>
+                <span
+                  className="border border-black px-2 py-1 rounded cursor-pointer"
+                  onClick={increaseQuantity}
+                >
+                  +
+                </span>
+              </div>
+            </div>
+            <button className="bg-accent w-full my-2 rounded-md p-2 text-white  font-normal">
+              Add to cart
+            </button>
           </div>
-          <button className="bg-accent w-full my-2 rounded-md p-2 text-white  font-normal">
-            Add to cart
-          </button>
         </div>
       </div>
 
       {/* Description and Review section  */}
-      <div className="rounded-md m-4 shadow-lg mb-8 lg:mx-20 border border-gray-200">
+      <div className="rounded-md m-4 shadow-lg mb-8 lg:w-3/4 lg:m-auto border border-gray-200">
         <section className="flex items-center justify-around border border-bottom shadow-md p-1 cursor-pointer">
           <div
             className={
@@ -94,11 +96,13 @@ const Details = () => {
           <section className="p-4 min-h-[200px] max-h-[400px] overflow-y-auto">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
             adipisci accusamus error expedita aut quia voluptatem officiis
-            eveniet, fugiat provident temporibus. Libero sit aperiam assumenda
-            ullam omnis quia doloremque ipsum.
+            eveniet, fugiat provident temporibus. <br /> Libero sit aperiam
+            assumenda ullam omnis quia doloremque ipsum. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Aliquam quidem tempora maxime
+            asperiores sunt magni quo nisi nemo cum cumque.
           </section>
         ) : (
-          <div className="px-4 my-4 min-h-[200px] max-h-[400px] overflow-y-auto">
+          <div className="px-8 my-4 min-h-[200px] max-h-[400px] overflow-y-auto">
             {/* post a review */}
             <div className="flex items-center gap-2 mb-4">
               <img
