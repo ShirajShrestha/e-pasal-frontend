4a0f36e502f69d358e528c06baf9218813dd7a1f
Shiraj Shrestha <shresthashiraj333@gmail.com>
2024-11-12 09:49:59 +0545

    Add Signup form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import SignUp from "../components/SignUp";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <SignUp />
     </>
   );
 };
