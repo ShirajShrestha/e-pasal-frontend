68f214be265993a665b162a28850ef677adaeb56
DikuCoding <dikeshkhagi1@gmail.com>
2024-11-14 13:24:59 +0545

    add cart component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,12 @@ import Filter from "../components/Filter";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Contact from "./Contact";
+import Cart from "../components/Cart";
 
 const Home = () => {
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
       <div className=" lg:max-h-[40vh] sm:max-h-[10vh] sm:overflow-hidden sm:w-screen ">
         <img src="https://images.unsplash.com/photo-1445384763658-0400939829cd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="lg:h-[40vh] object-cover overflow-hidden  w-full" />
       </div>
@@ -19,7 +20,9 @@ const Home = () => {
       <Card/>
       </div>
 
-      </div>
+      </div> */}
+
+      <Cart/>
       {/* <Footer/> */}
       
     </>
