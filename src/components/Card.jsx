169ae01f6a1e10e9bdd112214e0c8c6774a49340
DikuCoding <dikeshkhagi1@gmail.com>
2024-11-14 15:06:06 +0545

    add products page, make changes in card, filter

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,143 +1,29 @@
-import React from 'react'
-import "../App.css"
-import { FaLeftLong, FaRightLong } from "react-icons/fa6";
+import "../App.css";
 
-const Card = () => {
+const Card = ({ name, brand, price, image }) => {
   return (
-    <div className='cards'>
-      <div className="card-section flex flex-wrap justify-center items-center gap-[2.8rem] lg:my-16  lg:px-[8rem]">
-        <div className="card font-primary">
-            <img src="https://th.bing.com/th/id/OIP.ZZsn6lD6PCjocBzx1tuu1QHaEo?w=284&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" 
-            className='cardImage w-full h-56 overflow-hidden rounded-lg' alt="" />
-            <div className="card-body p-[0.3rem_0.01rem_0_0.3rem] leading-[1.3rem]">
-                <div className="card-title">
-                    <p className='card-brand text-sm font-normal text-gray-500'>Brand</p>
-                    <h5 className='card-title font-semibold'>Title</h5>
-                    <p className='card-price mt-[7px] text-[0.84rem] text-[#3095f9]'>Price:1000</p>
-                </div>
-
-            </div>
-        </div>
-        <div className="card sm:w-[5rem]">
-            <img src="https://th.bing.com/th/id/OIP.wExUhKWnGuDf-YkWPupnYQHaFj?w=233&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="sd" className='w-full h-56 overflow-hidden rounded-lg'/>
-            <div className="card-body p-[0.3rem_0.01rem_0_0.3rem] leading-[1.3rem]">
-                <div className="card-title">
-                    <p className='card-brand text-sm font-normal text-gray-500'>Brand</p>
-                    <h5 className='card-title font-semibold'>Title</h5>
-                    <p className='card-price mt-[7px] text-[0.84rem] text-[#3095f9]'>Price:1000</p>
-                </div>
-
-            </div>
-        </div>
-
-        <div className="card">
-            <img src="https://th.bing.com/th/id/OIP.ZZsn6lD6PCjocBzx1tuu1QHaEo?w=284&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="" className='w-full h-56 overflow-hidden rounded-lg'/>
-            <div className="card-body p-[0.3rem_0.01rem_0_0.3rem] leading-[1.3rem]">
-                <div className="card-title">
-                    <p className='card-brand text-sm font-normal text-gray-500'>Brand</p>
-                    <h5 className='card-title font-semibold'>Title</h5>
-                    <p className='card-price mt-[7px] text-[0.84rem] text-[#3095f9]'>Price:1000</p>
-                </div>
-
-            </div>
-        </div>
-
-        <div className="card">
-            <img src="https://th.bing.com/th/id/OIP.ZZsn6lD6PCjocBzx1tuu1QHaEo?w=284&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="" className='w-full h-56 overflow-hidden rounded-lg'/>
-            <div className="card-body p-[0.3rem_0.01rem_0_0.3rem] leading-[1.3rem]">
-                <div className="card-title">
-                    <p className='card-brand text-sm font-normal text-gray-500'>Brand</p>
-                    <h5 className='card-title font-semibold'>Title</h5>
-                    <p className='card-price mt-[7px] text-[0.84rem] text-[#3095f9]'>Price:1000</p>
-                </div>
-
-            </div>
-        </div>
-        <div className="card">
-        <img src="https://th.bing.com/th/id/OIP.ZZsn6lD6PCjocBzx1tuu1QHaEo?w=284&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="" className='w-full h-56 overflow-hidden rounded-lg'/>
-        <div className="card-body p-[0.3rem_0.01rem_0_0.3rem] leading-[1.3rem]">
-                <div className="card-title">
-                    <p className='card-brand text-sm font-normal text-gray-500'>Brand</p>
-                    <h5 className='card-title font-semibold'>Title</h5>
-                    <p className='card-price mt-[7px] text-[0.84rem] text-[#3095f9]'>Price:1000</p>
-                </div>
-
-            </div>
-        </div>
-
-        <div className="card">
-        <img src="https://th.bing.com/th/id/OIP.ZZsn6lD6PCjocBzx1tuu1QHaEo?w=284&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="" className='w-full h-56 overflow-hidden rounded-lg'/>
-        <div className="card-body p-[0.3rem_0.01rem_0_0.3rem] leading-[1.3rem]">
-                <div className="card-title">
-                    <p className='card-brand text-sm font-normal text-gray-500'>Brand</p>
-                    <h5 className='card-title font-semibold'>Title</h5>
-                    <p className='card-price mt-[7px] text-[0.84rem] text-[#3095f9]'>Price:1000</p>
-                </div>
-
-            </div>
-        </div>
-
-        <div className="card">
-        <img src="https://th.bing.com/th/id/OIP.ZZsn6lD6PCjocBzx1tuu1QHaEo?w=284&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="" className='w-full h-56 overflow-hidden rounded-lg'/>
-        <div className="card-body p-[0.3rem_0.01rem_0_0.3rem] leading-[1.3rem]">
-                <div className="card-title">
-                    <p className='card-brand text-sm font-normal text-gray-500'>Brand</p>
-                    <h5 className='card-title font-semibold'>Title</h5>
-                    <p className='card-price mt-[7px] text-[0.84rem] text-[#3095f9]'>Price:1000</p>
-                </div>
-
-            </div>
-        </div>
-
-        <div className="card">
-        <img src="https://th.bing.com/th/id/OIP.ZZsn6lD6PCjocBzx1tuu1QHaEo?w=284&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="" className='w-full h-56 overflow-hidden rounded-lg'/>
-        <div className="card-body p-[0.3rem_0.01rem_0_0.3rem] leading-[1.3rem]">
-                <div className="card-title">
-                    <p className='card-brand text-sm font-normal text-gray-500'>Brand</p>
-                    <h5 className='card-title font-semibold'>Title</h5>
-                    <p className='card-price mt-[7px] text-[0.84rem] text-[#3095f9]'>Price:1000</p>
-                </div>
-            </div>
-        </div>
+    <div className="h-52 w-38 md:h-72 md:w-48 shadow-lg">
+      <div className="h-3/4">
+        <img
+          src={image}
+          className="w-full h-full  rounded-lg"
+          alt=""
+        />
       </div>
 
-      {/* Pagination section */}
-      <div className="flex justify-center mt-4 space-x-2 my-4">
-      <button
-        // onClick={() => onPageChange(currentPage - 1)}
-        // disabled={currentPage === 1}
-        className="px-3 py-1 bg-gray-200 text-gray-600 rounded-md hover:bg-gray-300 disabled:opacity-50"
-      >
-        <FaLeftLong />
-      </button>
-
-        <button
-        className="px-3 py-1 bg-gray-200 text-gray-600 rounded-md hover:bg-gray-300 disabled:opacity-50"
-        >
-            1
-        </button>
-        <button
-        className="px-3 py-1 bg-gray-200 text-gray-600 rounded-md hover:bg-gray-300 disabled:opacity-50"
-        >
-            2
-        </button>
-        <button
-        className="px-3 py-1 bg-gray-200 text-gray-600 rounded-md hover:bg-gray-300 disabled:opacity-50"
-        >
-            3
-        </button>
-
-      {/* Next Button */}
-      <button
-        // onClick={() => onPageChange(currentPage + 1)}
-        // disabled={currentPage === totalPages}
-        className="px-3 py-1 bg-gray-200 text-gray-600 rounded-md hover:bg-gray-300 disabled:opacity-50"
-      >
-        <FaRightLong />
-      </button>
+      <div className="h-1/4">
+          <p className="text-sm text-gray-500 ml-6">
+            {brand}
+          </p>
+        <div className="flex justify-around items-center">
+          <h5 className="font-semibold">{name} </h5>
+          <p className="mt-[7px] text-[0.84rem] text-[#3095f9]">
+            Price:{price}
+          </p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
