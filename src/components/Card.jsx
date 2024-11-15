7f5474320aa1ea9791f22023ffadaf2e6eb83a3e
Shiraj Shrestha <shresthashiraj333@gmail.com>
2024-11-15 14:19:30 +0545

    Update homepage card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,17 +4,11 @@ const Card = ({ name, brand, price, image }) => {
   return (
     <div className="h-52 w-38 md:h-72 md:w-48 shadow-lg">
       <div className="h-3/4">
-        <img
-          src={image}
-          className="w-full h-full  rounded-lg"
-          alt=""
-        />
+        <img src={image} className="w-full h-full  rounded-lg" alt="" />
       </div>
 
       <div className="h-1/4">
-          <p className="text-sm text-gray-500 ml-6">
-            {brand}
-          </p>
+        <p className="text-sm text-gray-500 ml-6">{brand}</p>
         <div className="flex justify-around items-center">
           <h5 className="font-semibold">{name} </h5>
           <p className="mt-[7px] text-[0.84rem] text-[#3095f9]">
