dc8a8ab4ae4e8ef8aed73871cad9f47c6a8519c5
Shiraj Shrestha <shresthashiraj333@gmail.com>
2024-11-26 14:55:51 +0545

    Add loading state in sign in/up, product page and fix height of sign in page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -9,6 +9,7 @@ const SignIn = () => {
     email: "",
     password: "",
   });
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -23,6 +24,7 @@ const SignIn = () => {
     console.log("Login data submitted:", formData);
 
     try {
+      setLoading(true);
       const response = await signIn(formData);
       const user_data = response.user;
 
@@ -31,14 +33,16 @@ const SignIn = () => {
       navigate("/products");
     } catch (error) {
       console.error("Login failed:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div className="flex justify-center mt-2 bg-gray-100">
+    <div className="flex justify-center bg-gray-100">
       <form
         onSubmit={handleSubmit}
-        className="w-full max-w-md bg-white p-6 rounded-lg shadow-lg my-8"
+        className="w-full max-w-md bg-white p-6 rounded-lg shadow-lg lg:my-16 xl:my-32"
       >
         <h2 className="text-2xl font-bold text-center mb-4 text-tertiary">
           Sign In
@@ -79,7 +83,11 @@ const SignIn = () => {
           type="submit"
           className="w-full bg-accent text-white py-2 px-4 rounded hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-secondary focus:ring-opacity-50"
         >
-          Sign In
+          {loading ? (
+            <p className="cursor-not-allowed">Signing In</p>
+          ) : (
+            <p>Sign In</p>
+          )}
         </button>
 
         <p className="text-xs text-center mt-3">
